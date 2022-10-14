774dbb9d2e594ab2df64b5d148808a7b183f7250
Mauricio <105891765+JMBenitez-dev@users.noreply.github.com>
2022-10-14 13:29:53 -0300

    Update masajista.ts

--- selecciones/masajista.ts
+++ selecciones/masajista.ts
@@ -1,18 +1,18 @@
-import { Seleccion } from "./seleccion";
+import { Seleccion } from "./seleccion"; 
 import { Futbolista } from "./futbolista";
 
-export class Masajista extends Seleccion{
+export class Masajista extends Seleccion{ 
     private lesionDeFutbolista: any;
 
-   constructor(lesiones:any,futbolistas:Futbolista[],nombre:string,apellido:string,edad:number){
-     super();  
+   constructor(lesiones:any,futbolistas:Futbolista[],nombre:string,apellido:string,edad:number){ 
+     super();   
      this.lesionDeFutbolista = lesiones;
     }
-     getLesion():any{
+     getLesion():any{ 
         return this.lesionDeFutbolista;
      }
      setLesion(lesiones:string):void{
-        this.lesionDeFutbolista = lesiones;
+        this.lesionDeFutbolista = lesiones; 
      }
      tratarLesion(lesiones: any):void{
        for(let i:number = 0; i < this.lesionDeFutbolista.length; i++){
